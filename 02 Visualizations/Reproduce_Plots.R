cca6a839967e397a6c48d5a003bebc80601571ee
Ross Yudkin <RossYudkin@gmail.com>
2015-02-04 01:29:48 -0600

    no message

--- 02 Visualizations/Reproduce_Plots.R
+++ 02 Visualizations/Reproduce_Plots.R
@@ -1,8 +1,8 @@
 
 
-ggplot(dfReproduction, aes(x = as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(UNIT_PRICE)))+facet_wrap(~CUSTOMER_STATE)
+ggplot(dfReproduction, aes(x = as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(UNIT_PRICE)),na.rm=TRUE)+facet_wrap(~CUSTOMER_STATE)
 
-ggplot(dfReproduction, aes(x = as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(TITLE)))+facet_wrap(~CUSTOMER_STATE)
+ggplot(dfReproduction, aes(x = as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(TITLE)),na.rm=TRUE)+facet_wrap(~CUSTOMER_STATE)
 
-ggplot(dfReproduction, aes(x = as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(ARTIST)))+facet_wrap(~CUSTOMER_STATE)
+ggplot(dfReproduction, aes(x = as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(ARTIST)),na.rm=TRUE)+facet_wrap(~CUSTOMER_STATE)
 
